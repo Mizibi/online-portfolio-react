08044c9695708e1774a2f05bffb838231e5dcbb4
mizibi <k.dabard@gmail.com>
2020-01-31 16:06:58 +0100

    feat: init project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+type HomeProps = typeof Home.defaultProps & {
+  message: string;
+};
+
+type MyState = {
+  count: number;
+};
+  class Home extends React.Component<HomeProps, MyState> {
+    static defaultProps = {
+      name: "world"
+    };
+    state: MyState = {
+      count: 0
+    };
+
+    render() {
+      return (
+        <div>
+          {this.props.message} {this.state.count}
+        </div>
+      );
+    }
+  }
+
+    export {Home}
