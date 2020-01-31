604a49f7a3cda0ad475969bbc9218d159d227b6e
mizibi <k.dabard@gmail.com>
2020-01-31 16:44:52 +0100

    deps: add eslint and upgrade ts rules

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint"],
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    },
+  },
+  "rules": {
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+      { SwitchCase: 1, VariableDeclarator: 1, MemberExpression: 1 },
+    ],
+    'max-len': ['error', { code: 200 }],
+    'no-extra-parens': 'off',
+    'operator-linebreak': 'off',
+    'brace-style': 'off',
+    'curly': 'off',
+    'no-trailing-spaces': 'error',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'comma',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    "no-console": "warn",
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
+  }
+}
