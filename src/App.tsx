9156b66597e5e9cef537f9596bf82abe772f2126
mizibi <k.dabard@gmail.com>
2020-02-01 02:09:06 +0100

    feat: semantic ui react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button } from 'semantic-ui-react'
 
 const App = () => {
   return (
@@ -10,14 +11,8 @@ const App = () => {
         <p>
           Hello ! Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Button>Getting started</Button>
       </header>
     </div>
   )
